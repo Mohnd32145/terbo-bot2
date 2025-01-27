b770e846a053800e6cd61c3b7c448a489ae8bd1c
Mohnd32145 <mohndalcot@gmail.com>
2025-01-27 17:55:29 +0200

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -1,35 +1,14 @@
-import { googleImage } from '@bochilteam/scraper';
-
+import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `*[❗خطاء❗] مثال على الأمر ${usedPrefix + command} كانيكي*`;
-
-    // التحقق من الكلمات المحظورة
-    const forbiddenKeywords = ['gore', 'cp', 'porno', 'Gore', 'rule', 'CP', 'Rule34'];
-    if (forbiddenKeywords.some(keyword => m.text.includes(keyword))) {
-        return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى، المجموعة محظورة.\nإذا كنت مشرفًا وتريد تنشيطها، اخبر المطور');
-    }
-
-    // استعلام عن صورة
-    try {
-        // إضافة رد فعل مؤقت (🕐) قبل إرسال الصورة
-        await conn.sendMessage(m.chat, { react: { text: '🕐', key: m.key } });
-
-        const res = await googleImage(text);
-        const image = res[Math.floor(Math.random() * res.length)];
-        const link = image;
-
-        await conn.sendFile(m.chat, link, 'result.jpg', `🔎 *النتيجة ل:* ${text}\n🔗 *من* ${link}\n🌎 *محرك البحث:* جوجل`, m);
-
-        // إضافة رد فعل ناجح (✅️) بعد إرسال الصورة
-        await conn.sendMessage(m.chat, { react: { text: '✅️', key: m.key } });
-    } catch (error) {
-        console.error(error);
-        m.reply('[❗خطاء❗] حدث خطأ أثناء جلب الصورة، حاول مرة أخرى لاحقاً.');
-    }
-};
-
-handler.help = ['gimage <query>', 'imagen <query>'];
-handler.tags = ['internet', 'tools'];
-handler.command = /^(gimage|image|صوره|imagen)$/i;
-
-export default handler;
+if (!text) throw `*فين النص اللي هتبحث عنو ي حوب 🧞* \n> *مثال علي الامر ${usedPrefix + command} ناروتو*`
+//if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى ، المجموعة محظورة \n إذا كنت مشرفًا وتريد تنشيطها ، اخبر المطور')  
+const res = await googleImage(text)
+let image = await res.getRandom()
+let link = image
+//conn.sendFile(m.chat, link, 'error.jpg', `🔎 *النتيجه ل:* ${text}\n🔗 *من* ${link}\n🌎 *محرك البحث:* جوجل`, m)}
+let captionn = `🔎 *الـبـحـث عـن:* ${text}\n🔗 *الـليـنـك* ${link}\n🌎 *مـحـرـك الـبـحـث:* Google`
+conn.sendButton(m.chat, captionn, author, link, [['التـــالي', `${usedPrefix + command} ${text}`],['الاوامــر',`${usedPrefix}منيو`]], m)}
+handler.help = ['صورة <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(صورة|صوره)$/i
+export default handler
